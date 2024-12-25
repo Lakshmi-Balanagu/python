6123255dfe94384acee3a04a1ece6ab6a5921f7e
Lakshmi-Balanagu <lakshmi.balanagu12@gmail.com>
2024-12-25 18:11:15 +0000

    basic string examples added

---
+++ datatypes/Examples/print_f.py
@@ -0,0 +1,11 @@
+english = 78
+maths = 56
+hindi = 85
+
+print(f"Ram got total marks {english + maths + hindi} out of 300")
+
+# Printing single braces
+print(f"{{Hello, Geek}}")
+
+# Printing double braces
+print(f"{{{{Hello, Geek}}}}")
--- read_input.py
+++ read_input.py
@@ -1,15 +1,2 @@
-val1 = input("Enter your value: ")
-val2 = input("Enter your value: ")
-add = val1+val2
-print("addition of 2 values:", add) #o/p will come like string conactination 3,4 add=34
-
-val1 = float(input("Enter your value: "))
-val2 = int(input("Enter your value: "))
-add = float(val1+val2)
-print("addition of 2 values:", round(add)) #o/p will come addition.3,4 ,add=7
-
-english = 78
-maths = 56
-hindi = 85
-
-print(f"Ram got total marks {english + maths + hindi} out of 300")
+val1 = input("Enter your Name: ")
+print("Hi",val1," How are you") 
