c4481556b6c5f0d448b1da2b3cd99f167daeab4a
Lakshmi-Balanagu <lakshmi.balanagu12@gmail.com>
2024-12-25 18:05:42 +0000

    basic string examples added

--- read_input.py
+++ read_input.py
@@ -7,3 +7,9 @@ val1 = float(input("Enter your value: "))
 val2 = int(input("Enter your value: "))
 add = float(val1+val2)
 print("addition of 2 values:", round(add)) #o/p will come addition.3,4 ,add=7
+
+english = 78
+maths = 56
+hindi = 85
+
+print(f"Ram got total marks {english + maths + hindi} out of 300")
