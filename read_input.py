94f82a16e6ce580c8d893b7e52495bd882ac1141
Lakshmi-Balanagu <lakshmi.balanagu12@gmail.com>
2024-12-25 17:58:55 +0000

    basic string examples added

---
+++ read_input.py
@@ -0,0 +1,9 @@
+val1 = input("Enter your value: ")
+val2 = input("Enter your value: ")
+add = val1+val2
+print("addition of 2 values:", add) #o/p will come like string conactination 3,4 add=34
+
+val1 = float(input("Enter your value: "))
+val2 = int(input("Enter your value: "))
+add = float(val1+val2)
+print("addition of 2 values:", round(add)) #o/p will come addition.3,4 ,add=7
